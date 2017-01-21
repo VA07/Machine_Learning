dcf0e765e97963e4643ae3dd50f328f1ac3d3e0a
Vignesh A <Vignesh A>
2017-01-21 22:49:18 +0530

    remove outlier

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,9 +12,29 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
+    out_perc = 10 #perecntage of outliers
+	
+	#convert to lists for ease of operation
+    age = ages.tolist()
+    pred = predictions.tolist()
+    net_worth = net_worths.tolist()	
+	
+	#Calculate the residual errors
+    res_error = []
+    for i in xrange(len(predictions)):
+        res_error.append(net_worths[i] - predictions[i])
 
-    ### your code goes here
-
+    for i in xrange(len(pred)*out_perc/100):
+        ind = res_error.index(min(res_error))
+        print ind , res_error[ind]
+        del res_error[ind]
+        del age[ind]
+        del net_worth[ind]	 
+  
     
+    for i in xrange(len(age) ):
+	    cleaned_data.append(([age[i]],[net_worth[i]], [res_error[i]]))
+
     return cleaned_data
+
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,14 +25,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+pred = reg.predict(ages_test)
 
-
-
-
-
-
-
-
+	 
+print "Slope = ", reg.coef_
+print "score = ", reg.score(ages_test, net_worths_test)
 
 
 
@@ -68,6 +68,8 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "Slope_new = ", reg.coef_
+        print "score_new = ", reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
