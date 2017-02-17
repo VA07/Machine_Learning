4c00604c5fe37a0952d7b6075f893162744f01aa
Vignesh A <Vignesh A>
2017-02-17 18:06:50 +0530

    add validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,22 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
 
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+clf = DecisionTreeClassifier()
+
+clf.fit(x_train, y_train)
+
+
+pred = clf.predict(x_test)
+
+accuracy = accuracy_score(y_test, pred)
+print "accuracy = ", accuracy*100, "%"
 
 
